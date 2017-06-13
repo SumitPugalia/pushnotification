29f67c7599057861837c16dd12fe04aa1ede6b5f
SumitForYou <sumit_pugalia@yahoo.com>
2017-06-13 08:27:15 +0530

    TEsting Git

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-## Replace promotion@127.0.0.1 with promotion@ip-address (nodename)...
+##  Replace promotion@127.0.0.1 with promotion@ip-address (nodename)...
 run_erl -daemon /tmp/pushnotification /tmp/log_dir/ "erl -pa ./ebin -pa ./lib/*/ebin -name promotion@127.0.0.1 -setcookie emq_dist_cookie"
 
 
